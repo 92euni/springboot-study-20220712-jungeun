ba2874098dae316744083ae038af2f024b2b91e6
92euni <92_sudal@naver.com>
2022-07-13 17:04:53 +0900

    init commit

---
+++ src/main/java/com/springboot/studyjungeun/StudyJungeunApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.studyjungeun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJungeunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyJungeunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/studyjungeun/StudyJungeunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.studyjungeun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyJungeunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
